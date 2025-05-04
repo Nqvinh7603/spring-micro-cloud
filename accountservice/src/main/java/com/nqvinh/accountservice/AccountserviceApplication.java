0b53ada70cbbf78d252fa06b7975ad00d22092e0
Nqvinh7603 <nqvinh7603@gmail.com>
2025-05-05 00:01:29 +0700

    add init accservice

---
+++ accountservice/src/main/java/com/nqvinh/accountservice/AccountserviceApplication.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Class        ：AccountserviceApplication
+ * Created date ：2025/05/04
+ * Lasted date  ：2025/05/04
+ * Author       ：vinhNQ2
+ * Change log   ：2025/05/04：01-00 vinhNQ2 create a new
+ ******************************************************************************/
+package com.nqvinh.accountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * AccountserviceApplication
+ * 
+ * @version 01-00
+ * @since 01-00
+ * @author vinhNQ2
+ */
+@SpringBootApplication
+public class AccountserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountserviceApplication.class, args);
+	}
+
+}
